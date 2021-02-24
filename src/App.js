ab69276c0f47d24728a26ba962334b684b435f5c
Diego Rubio <darubiomunoz@gmail.com>
2021-02-24 17:58:46 -0300

    illustration positioning

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 
+import illustration from './assets/images/illustration-woman-online-desktop.svg';
+
 function App() {
   return (
     <div className="app">
+      <img className="app__illustration" src={illustration} alt="Digital illustration of a woman looking at a computer screen"/>
       hi
     </div>
   );
