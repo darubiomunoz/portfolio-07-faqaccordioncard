18fdd337cb6f5ce05659420300e190a96f0710e2
Diego Rubio <darubiomunoz@gmail.com>
2021-02-24 22:13:07 -0300

    faq component set up

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 
 import Illustration from './components/Illustration';
 import Title from './components/Title';
+import FAQ from './components/FAQ';
 
 function App() {
   return (
     <div className="app">
       <Illustration />
       <Title />
+      <FAQ />
     </div>
   );
 }
---
+++ src/components/FAQ.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class FAQ extends React.Component {
+    render(
+        return (
+            faq
+        );
+    )
+}
+
+export default FAQ;
