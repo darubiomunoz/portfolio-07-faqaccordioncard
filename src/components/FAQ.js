24257828fd27c5a0970ffd51c5c3962df98de207
Diego Rubio <darubiomunoz@gmail.com>
2021-02-26 16:09:59 -0300

    correction the layout and component styles

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import './styles/FAQ.css';
 
-class FAQ extends React.Component {
-    render(
-        return (
-            faq
-        );
-    )
+import data from '../data.json';
+import arrow from '../assets/images/icon-arrow-down.svg';
+
+const FAQ = () => {
+    return (
+        <div className="faq">
+            { data.map( (data, i) => {
+                return (
+                    <div key={i} className="faq__item">
+                        <h2 className="faq__question">{data.question}<img className="faq__icon" src={arrow} alt="arrow icon to show the answer"/></h2>
+                        {false && <p className="faq__answer">{data.answer}</p>}
+                    </div>
+                )})
+            }
+        </div>
+    );
 }
 
 export default FAQ;
