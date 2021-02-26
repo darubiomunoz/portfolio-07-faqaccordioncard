4234b4fd882cef38e63a0edc7ffa8f278d7772f9
Diego Rubio <darubiomunoz@gmail.com>
2021-02-26 19:07:09 -0300

    Rendering answer if active and adding styles on active

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/FAQ.css';
 
 import data from '../data.json';
 import arrow from '../assets/images/icon-arrow-down.svg';
 
+
 const FAQ = () => {
+    const [ active, setActive ] = useState(false);
+
+    const activation = () => {
+        const question = document.querySelector('.faq__question');
+        if(active) {
+            question.className = 'faq__question'
+        } else if(!active) {
+            question.className += ' active';
+        }
+    }
+
+    const handleClick = event => {
+        if(active) {
+            setActive(false);
+            activation();
+        } else if(!active) {
+            setActive(true);
+            activation();
+        }
+    }
+
     return (
         <div className="faq">
             { data.map( (data, i) => {
                 return (
-                    <div key={i} className="faq__item">
+                    <div key={i} className="faq__item" onClick={handleClick}>
                         <h2 className="faq__question">{data.question}<img className="faq__icon" src={arrow} alt="arrow icon to show the answer"/></h2>
-                        {false && <p className="faq__answer">{data.answer}</p>}
+                        {active && <p className="faq__answer">{data.answer}</p>}
                     </div>
                 )})
             }
