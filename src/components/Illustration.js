c7ef9bb4a2098d452359bd5aa65d004e89f57d28
Diego Rubio <darubiomunoz@gmail.com>
2021-02-28 19:25:06 -0300

    Layout and design achieved for desktop devices

--- src/components/Illustration.js
+++ src/components/Illustration.js
@@ -3,14 +3,15 @@ import './styles/Illustration.css';
 
 import box from '../assets/images/illustration-box-desktop.svg';
 import illustration from '../assets/images/illustration-woman-online-desktop.svg';
-import shadow from '../assets/images/bg-pattern-mobile.svg';
+import shadowMobile from '../assets/images/bg-pattern-mobile.svg';
+import shadowDesktop from '../assets/images/bg-pattern-desktop.svg';
 
 function Illustration() {
     return (
         <div className="illustration">
             <img className="illustration__box" src={box} alt="Digital illustration of a box"/>
             <img className="illustration__base" src={illustration} alt="Digital illustration of a woman looking at a computer screen"/>
-            <img className="illustration__shadow" src={shadow} alt="Digital illustration of the a shadow"/>
+            <img className="illustration__shadow" src={shadowMobile} srcset={shadowDesktop + " 1025w"} sizes="(min-width: 1025px) 1025px" alt="Digital illustration of a shadow"/>
         </div>
     );
 }
