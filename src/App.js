9397d1a1be604e007cf5525fd9e7b779dd23a093
Diego Rubio <darubiomunoz@gmail.com>
2021-04-03 00:08:51 -0300

    Deleting the FAQ component that wasn't needed at all I went to far with 'Divide and Conquer'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 
 import Illustration from "./components/Illustration";
-import FAQ from "./components/FAQ";
 import Items from "./components/Items";
 
 function App() {
@@ -9,9 +8,12 @@ function App() {
     <div className="app">
       <div className="app__container">
         <Illustration />
-        <FAQ>
-          <Items />
-        </FAQ>
+        <ul className="faq">
+          <h1 className="title" tabIndex="0">
+            FAQ
+          </h1>
+        </ul>
+        <Items />
       </div>
     </div>
   );
