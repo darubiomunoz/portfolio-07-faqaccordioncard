a76a6579852ac39b68750364491ce4d981a9199d
Diego Rubio <darubiomunoz@gmail.com>
2021-04-03 00:01:07 -0300

    Fixing styles of component - devices under 500px of width are ok!

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
+import "./App.css";
 
-import Illustration from './components/Illustration';
-import FAQ from './components/FAQ';
-import Items from './components/Items';
+import Illustration from "./components/Illustration";
+import FAQ from "./components/FAQ";
+import Items from "./components/Items";
 
 function App() {
   return (
     <div className="app">
-      <Illustration />
-      <FAQ>
-        <Items />
-      </FAQ>
+      <div className="app__container">
+        <Illustration />
+        <FAQ>
+          <Items />
+        </FAQ>
+      </div>
     </div>
   );
 }
